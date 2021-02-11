1837311cf90ad5c0a18a06868ca96115d58a1aa3
Wapps <wapps3@gmail.com>
2021-02-11 17:05:40 +0100

    Add shooting script which handle fire rate

---
+++ Assets/Scripts/Control/ShootController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    public float maxFireRate;
+    private float lastFireTime = 0;
+
+    public GameObject bulletPrefab;
+
+    public GameObject fisrtWeapon;
+    public GameObject secondWeapon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFireTime = maxFireRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastFireTime += Time.deltaTime;
+
+        if (Input.GetButton("Fire"))
+        {
+            if(lastFireTime >= maxFireRate)
+            {
+                Instantiate(bulletPrefab, fisrtWeapon.gameObject.transform.position, fisrtWeapon.gameObject.transform.rotation);
+                Instantiate(bulletPrefab, secondWeapon.gameObject.transform.position, secondWeapon.gameObject.transform.rotation);
+
+                lastFireTime = 0;
+            }
+        }
+    }
+}
