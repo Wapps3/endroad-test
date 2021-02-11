293b450d7badc31d0f0d40b98e9ea3fd17ccd91e
Wapps <wapps3@gmail.com>
2021-02-11 17:04:36 +0100

    Simple class to handle life point

---
+++ Assets/Scripts/Stat/StatManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    public float maxLife;
+    private float currentLife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLife = maxLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool Damage(float damage)
+    {
+        currentLife -= damage;
+
+        if(currentLife <= 0)
+        {
+            Destroy(gameObject);
+            return true;
+        }
+
+        return false;
+    }
+}
