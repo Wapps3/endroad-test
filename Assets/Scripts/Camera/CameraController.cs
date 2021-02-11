8d13e22a70cacaa800390e4a524ab18aad232fcf
Wapps <wapps3@gmail.com>
2021-02-11 12:49:15 +0100

    Camera follow jet with a little lerp

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float lerpSpeed;
+
+    public Transform objectToFollow;
+
+    public float cameraPitch;
+    public float cameraDistance;
+
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0, Mathf.Sin(cameraPitch), Mathf.Cos(cameraPitch)) * cameraDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, objectToFollow.position - offset, Time.deltaTime * lerpSpeed);
+    }
+}
