afdf5b84dcac9edb8dfbdf579d1fb4168811dea0
Wapps <wapps3@gmail.com>
2021-02-11 17:05:06 +0100

    Add bullet script and add it to the prefab

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime < 0)
+            Destroy(gameObject);
+
+        gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<StatManager>())
+        {
+            collision.gameObject.GetComponent<StatManager>().Damage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
